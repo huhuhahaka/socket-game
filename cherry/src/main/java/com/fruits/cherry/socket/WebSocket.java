d344230bbb7c7674107540246469111d16b0b514
ggl <shs-wlfzzyggl@chinaunicom.cn>
2020-06-05 18:27:17 +0800

    消息广播

--- cherry/src/main/java/com/fruits/cherry/socket/WebSocket.java
+++ cherry/src/main/java/com/fruits/cherry/socket/WebSocket.java
@@ -10,6 +10,8 @@ import javax.websocket.OnOpen;
 import javax.websocket.Session;
 import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Slf4j
@@ -17,48 +19,41 @@ import java.util.concurrent.ConcurrentHashMap;
 @ServerEndpoint("/websocket/{name}")
 public class WebSocket {
 
-    /**
-     *  与某个客户端的连接对话，需要通过它来给客户端发送消息
-     */
-    private Session session;
-
-    /**
-     * 标识当前连接客户端的用户名
-     */
-    private String name;
 
     /**
      *  用于存所有的连接服务的客户端，这个对象存储是安全的
      */
-    private static ConcurrentHashMap<String,WebSocket> webSocketSet = new ConcurrentHashMap<>();
+    private static ConcurrentHashMap<String, List<Session>> webSocketSet = new ConcurrentHashMap<>();
 
 
     @OnOpen
     public void OnOpen(Session session, @PathParam(value = "name") String name){
-        this.session = session;
-        this.name = name;
-        // name是用来表示唯一客户端，如果需要指定发送，需要指定发送通过name来区分
-        webSocketSet.put(name,this);
-        log.info("[WebSocket] 连接成功，当前连接人数为：={}",webSocketSet.size());
+        if(webSocketSet.get(name) == null){
+            List<Session> sessionList = new ArrayList<>();
+            sessionList.add(session);
+            webSocketSet.put(name, sessionList);
+        }else {
+            webSocketSet.get(name).add(session);
+        }
+        log.info("房间 {} ，人数为 {}。",name,webSocketSet.get(name).size());
     }
 
 
     @OnClose
-    public void OnClose(){
-        webSocketSet.remove(this.name);
-        log.info("[WebSocket] 退出成功，当前连接人数为：={}",webSocketSet.size());
+    public void OnClose(Session session){
+        log.info("{} 退出房间。",session.getId());
     }
 
     @OnMessage
     public void OnMessage(String message){
-        log.info("[WebSocket] 收到消息：{}",message);
-        //判断是否需要指定发送，具体规则自定义
-        if(message.indexOf("TOUSER") == 0){
-            String name = message.substring(message.indexOf("TOUSER")+6,message.indexOf(";"));
-            AppointSending(name,message.substring(message.indexOf(";")+1,message.length()));
-        }else{
+//        log.info("[WebSocket] 收到消息：{}",message);
+//        //判断是否需要指定发送，具体规则自定义
+//        if(message.indexOf("TOUSER") == 0){
+//            String name = message.substring(message.indexOf("TOUSER")+6,message.indexOf(";"));
+//            AppointSending(name,message.substring(message.indexOf(";")+1,message.length()));
+//        }else{
             GroupSending(message);
-        }
+//        }
 
     }
 
@@ -69,23 +64,15 @@ public class WebSocket {
     public void GroupSending(String message){
         for (String name : webSocketSet.keySet()){
             try {
-                webSocketSet.get(name).session.getBasicRemote().sendText(message);
+                List<Session> sessionList = webSocketSet.get(name);
+                for (Session session: sessionList){
+                    session.getBasicRemote().sendText(message);
+                }
             }catch (Exception e){
                 e.printStackTrace();
             }
         }
     }
 
-    /**
-     * 指定发送
-     * @param name
-     * @param message
-     */
-    public void AppointSending(String name,String message){
-        try {
-            webSocketSet.get(name).session.getBasicRemote().sendText(message);
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-    }
+
 }
